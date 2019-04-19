a8d9dbc99cbece170d682fc74a0601d3529baaf6
cy4v <27627468+cy4v@users.noreply.github.com>
2019-04-19 23:11:17 +0900

    Update mango.js

--- src/mango.js
+++ src/mango.js
@@ -1,7 +1,15 @@
-$(function () {
-	
-	$(".ndPaywall").attr("style", "display:none");
-	$(".ndAritcle_headPic").removeAttr("style");
-	$(".ndArticle_margin").removeAttr("style");
-
+$.ajax({
+	success: function (data) {
+		
+		var ndAritcle_headPic = $('.ndAritcle_headPic', $(data));
+		var ndArticle_margin = $('.ndArticle_margin', $(data));
+		
+		$('.thoracis').prepend(ndAritcle_headPic);
+		$('.ndArticle_content').prepend(ndArticle_margin);
+		
+		$(".ndPaywall").attr("style", "display:none");
+		$(".ndAritcle_headPic").removeAttr("style");
+		$(".ndArticle_margin").removeAttr("style");
+		
+	}
 });
